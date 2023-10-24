1d2d80c1d693b361c0f5659568d1b753c0fe743a
Abhishek Kumar <ak0188644@gmail.com>
2023-10-24 19:34:34 +0530

    v1.2

--- smashIT/settings.py
+++ smashIT/settings.py
@@ -125,7 +125,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'convert/static'),
-    os.path.join(BASE_DIR, 'static'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
