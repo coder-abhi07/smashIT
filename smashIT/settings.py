55dbcc3b1ea3c51b383405bfda27be5833bdc749
Abhishek Kumar <ak0188644@gmail.com>
2023-10-26 22:43:25 +0530

    v1.33

--- smashIT/settings.py
+++ smashIT/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS=[
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
