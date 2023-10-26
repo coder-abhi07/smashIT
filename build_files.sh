e2867339d60c4ab8cf3dad1742c4dd86be05ef68
Abhishek Kumar <ak0188644@gmail.com>
2023-10-26 12:19:31 +0530

    updated requirements.txt

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,4 @@
 source my_venv/Scripts/activate
-sudo apt update
-sudo apt install postgresql postgresql-contrib
-sudo systemctl start postgresql.service
 # Add PostgreSQL binaries to the system PATH
 #echo 'export PATH=$PATH:/usr/pgsql-13/bin' >> ~/.bashrc
 #source ~/.bashrc
