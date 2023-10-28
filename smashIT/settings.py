69aac605301e7c04491e8a030dd764ef1d365f93
Abhishek Kumar <ak0188644@gmail.com>
2023-10-28 23:31:05 +0530

    collect static again

--- smashIT/settings.py
+++ smashIT/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-57jli0q*63+_&m8)1_*8z%(-d56=jk$i7%ye7(o&b$d3&r35l&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com', 'smashit.tech', 'www.smashit.tech', 'smashit.onrender.com']
+ALLOWED_HOSTS = ["*",'.onrender.com', 'smashit.tech', 'www.smashit.tech', 'smashit.onrender.com']
 
 
 # Application definition
