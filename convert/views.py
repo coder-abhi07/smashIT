b976417a68ef6aa7279c1c7387feafab80091ca8
Abhishek Kumar <ak0188644@gmail.com>
2023-11-03 20:46:51 +0530

    Update views.py

--- convert/views.py
+++ convert/views.py
@@ -170,6 +170,7 @@ def custom_page_not_found_view(request, exception):
     return render(request, "404.html", {'error_message': error_message}, status=404)
 
 def custom_error_view(request, exception=None):
+    logger.error('internal server error: %s', request.path)
     return render(request, "500.html", {})
 
 def custom_permission_denied_view(request, exception=None):
