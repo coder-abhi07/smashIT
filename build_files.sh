48d5cba3a17e94ad742369629f375d129f39559b
Abhishek Kumar <68725300+coder-abhi07@users.noreply.github.com>
2024-10-27 11:45:55 +0530

    Update build_files.sh
    Signed-off-by: Abhishek Kumar <68725300+coder-abhi07@users.noreply.github.com>

--- build_files.sh
+++ build_files.sh
@@ -3,3 +3,4 @@ python3 -m pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
+python3 -m pip install --upgrade pip
