bd54931b5659d92613d1a496da30e0b457d5d724
Abhishek Kumar <68725300+coder-abhi07@users.noreply.github.com>
2024-11-08 17:31:44 +0530

    Update urls.py
    Signed-off-by: Abhishek Kumar <68725300+coder-abhi07@users.noreply.github.com>

--- convert/urls.py
+++ convert/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     
     path("", views.index, name="index"),
     path("textResponse", views.textResponse, name="textResponse"),
-    path("login", views.login, name="login"),
-    path("logout", views.logout, name="logout"),
-    path("callback", views.callback, name="callback"),
+    #path("login", views.login, name="login"),
+    #path("logout", views.logout, name="logout"),
+   # path("callback", views.callback, name="callback"),
     
     path('result', views.finalResult, name='result'),
     path('about', views.about, name='about'),
