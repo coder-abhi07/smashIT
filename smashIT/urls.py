9d956278730248a99bde1ce6b0deecebbc51ef99
coder-abhi07 <ak0188644@gmail.com>
2025-03-27 13:27:53 +0530

    adsense

--- smashIT/urls.py
+++ smashIT/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('', include('convert.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path("robots.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    path('ads.txt', TemplateView.as_view(template_name="ads.txt", content_type="text/plain")),
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('static/icon/favicon.ico')))
 ]
 
