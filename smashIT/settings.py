b7f58cac1f7574ab883823d8eedb80a182f23359
coder-abhi07 <ak0188644@gmail.com>
2025-03-27 12:27:31 +0530

    SEO

--- smashIT/settings.py
+++ smashIT/settings.py
@@ -4,24 +4,19 @@ from dotenv import load_dotenv
 from urllib.parse import urlparse
 
 load_dotenv()
-SECURE_SSL_REDIRECT = True
 
-ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https"
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG').lower() == 'true'
+DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
 
 ALLOWED_HOSTS = ["*"]
 
-SITE_ID = 1
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SITE_ID = 9
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -223,3 +218,13 @@ CSRF_TRUSTED_ORIGINS = [
 SOCIALACCOUNT_ADAPTER = "convert.adapters.CustomSocialAccountAdapter"
 
 AUTH_USER_MODEL = 'convert.CustomUser'
+
+#production
+
+IS_PRODUCTION = os.getenv("DJANGO_PRODUCTION", "true").lower() == "true"
+
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https" if IS_PRODUCTION else "http"
+SECURE_SSL_REDIRECT = IS_PRODUCTION
+CSRF_COOKIE_SECURE = IS_PRODUCTION
+SESSION_COOKIE_SECURE = IS_PRODUCTION
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') if IS_PRODUCTION else None
